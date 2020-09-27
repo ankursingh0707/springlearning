154698f65306cc65023cae869601eed537422cea
SINGH Ankur <ankur.singh@soprasteria.com>
2020-09-27 15:12:35 +0530

    initail value for microservices

---
+++ mssc-beer-service/src/test/java/spring/learning/msscbeer/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.learning.msscbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
